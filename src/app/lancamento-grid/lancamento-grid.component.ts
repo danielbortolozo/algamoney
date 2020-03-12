2e09a23ef7edd1341da769d7e3e0c1b2ff56e56d
julio.costa <daniel.bortolozo@opah.com.br>
2020-03-12 12:16:10 -0300

    componente grid

---
+++ src/app/lancamento-grid/lancamento-grid.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-grid',
+  templateUrl: './lancamento-grid.component.html',
+  styleUrls: ['./lancamento-grid.component.css']
+})
+export class LancamentoGridComponent  {
+
+  @Input() lancamentos =  [];
+
+
+}
