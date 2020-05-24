2b8eb82e6f5cfe69d621f63557f52980e4f3b6b3
Daniel Bortolozo <daniel.bortolozo@opah.com.br>
2020-05-24 19:56:17 -0300

    Pesquisa paginacao lancamentos 1

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -8,14 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PessoasPesquisaComponent  {
 
   pessoas = [
-    {nome: 'Vera Marcia R. Bortolozo',  cidade: 'Fernandópolis', estado: 'SP', status: 'Ativo'},
-    {nome: 'Mauro Bortolozo Junior',  cidade: 'Macedônia', estado: 'SP', status: 'Ativo'},
-    {nome: 'Daianne Rocca Bortolozo',  cidade: 'São João Das Duas Pontes', estado: 'SP', status: 'Ativo'},
-    {nome: 'Daniel Rocca Bortolozo',  cidade: 'São Paulo', estado: 'SP', status: 'Ativo'},
-    {nome: 'Vitor Rocca Bortolozo',  cidade: 'São José do Rio Preto', estado: 'SP', status: 'Inativo'},
-    {nome: 'Rodrigo Scavone',  cidade: 'São João Das Duas Pontes', estado: 'SP', status: 'Inativo'},
-    {nome: 'Willia Sanaiotte Rocca',  cidade: 'São José do Rio Preto', estado: 'SP', status: 'Ativo'},
-    {nome: 'Nelson Rocca',  cidade: 'São José do Rio Preto', estado: 'SP', status: 'Inativo'}
+
 
   ];
 
