c36a604f772e048ce8077b9b5bd0e2f22d0060ec
Daniel Bortolozo <daniel.bortolozo@opah.com.br>
2020-05-31 01:36:10 -0300

    Fazendo o redirecionamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
    { path: 'lancamentos', component: LancamentosPesquisaComponent },
    { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
    { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
