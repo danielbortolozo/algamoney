e2061870568d9a6aac0c440483f695a52fc53cd5
Daniel Bortolozo <daniel.bortolozo@opah.com.br>
2020-05-31 02:29:01 -0300

    Refatorando as rotas para usar Routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { CategoriaModule } from './categoria/categoria.module';
 
 import { CoreModule } from './core/core.module';
@@ -17,20 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import { Routes, RouterModule } from '@angular/router';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-   { path: 'lancamentos', component: LancamentosPesquisaComponent },
-   { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-   { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent },
-   { path: 'pessoas', component: PessoasPesquisaComponent},
-   { path: 'pessoas/novo', component: PessoaCadastroComponent },
-   { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-   { path: '**', redirectTo: 'pagina-nao-encontrada' },
-];
+
 
 registerLocaleData(localePt);
 
@@ -47,7 +34,8 @@ registerLocaleData(localePt);
     CategoriaModule,
     CoreModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
+
 
   ],
   providers: [
