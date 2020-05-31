86285f6d500348e6b6121c0d96640c036419119d
Daniel Bortolozo <daniel.bortolozo@opah.com.br>
2020-05-31 20:39:36 -0300

    Adicionando title, e edicao de pessoa

--- src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -4,20 +4,36 @@ import { FormControl } from '@angular/forms';
 import { PessoaService } from '../pessoa.service';
 import { ToastyService } from 'ng2-toasty';
 import { Pessoa } from 'src/app/core/model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pessoa-cadastro',
   templateUrl: './pessoa-cadastro.component.html',
   styleUrls: ['./pessoa-cadastro.component.css']
 })
-export class PessoaCadastroComponent  {
+export class PessoaCadastroComponent implements OnInit {
 
   pessoa = new Pessoa();
 
   constructor(private pessoaService: PessoaService,
               private toasty: ToastyService,
-              private errorHandler: ErrorHandlerService) {}
+              private errorHandler: ErrorHandlerService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private title: Title) {}
 
+  ngOnInit() {
+    this.title.setTitle('Novo lançamento');
+    const codigoPessoa = this.route.snapshot.params['codigo'];
+
+    if (codigoPessoa) {
+        this.carregarPessoa(codigoPessoa);
+    }
+  }
+  get editando() {
+    return Boolean(this.pessoa.codigo);
+  }
 
   salvar(form: FormControl) {
 
@@ -29,4 +45,29 @@ export class PessoaCadastroComponent  {
      })
      .catch(erro => this.errorHandler.handle(erro));
   }
+
+  atualizarPessoa(form: FormControl) {
+
+    this.pessoaService.atualizar(this.pessoa)
+    .then(pessoa => {
+      this.pessoa = pessoa;
+      this.toasty.success('Pessoa alterado com sucesso !!!');
+      this.editarTituloEdicao();
+    })
+    .catch(erro => this.errorHandler.handle(erro));
+ }
+
+  carregarPessoa(codigo: number) {
+    this.pessoaService.buscarPorCodigo(codigo)
+    .then(pessoa => {
+       this.pessoa = pessoa;
+       this.editarTituloEdicao();
+    })
+    .catch(erro => this.errorHandler.handle(erro));
+ }
+
+ editarTituloEdicao() {
+  this.title.setTitle(`Ediçao de lançamento: ${this.pessoa.nome}`);
+}
+
 }
